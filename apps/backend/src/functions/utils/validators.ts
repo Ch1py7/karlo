2a662eb5b50e6b3a2a10e3b25b34ddd9bbc7bd24
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-11 04:30:52 -0600

    add validation and authentication utilities for email and JWT tokens

---
+++ apps/backend/src/functions/utils/validators.ts
@@ -0,0 +1,36 @@
+import { verifyJwt } from '../service/jwt'
+import type express from 'express'
+
+export const validateEmail = (email: string) => {
+	const regex =
+		// biome-ignore lint/performance/useTopLevelRegex: <explanation>
+		// biome-ignore lint/suspicious/noControlCharactersInRegex: <explanation>
+		/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+	return !regex.test(email)
+}
+
+export const isTokenValid = (exp: number) => {
+  return Date.now() > exp * 1000
+}
+
+export const authenticate = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	const token = req.headers.authorization?.split(' ')[1] ?? ''
+	try {
+		const payload = verifyJwt<{ sub: number; exp: number; role_id: 1 | 2; is_validated: boolean }>(
+			token
+		)
+		if (Date.now() > payload.exp * 1000) {
+			return res.status(401).json({ errors: ['token expired'] })
+		}
+		if (!payload.is_validated) {
+			return res
+				.status(401)
+				.json({ errors: ['Please validate your account before performing this action'] })
+		}
+
+		next()
+	} catch (e) {
+		res.status(400).json({ errors: [(e as Error).message] })
+	}
+}
